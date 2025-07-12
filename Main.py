704dd4c2b6519761792910d850f1e73697643020
The Prime Mathematician <197593373+Prime-Lasking@users.noreply.github.com>
2025-07-12 12:47:18 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,31 +1,31 @@
 from Evaluation import *
 import chess
-board = chess.Board()
-while not board.is_game_over():
-    print(board)
-    print('--------------------')
-    if board.turn:
-        move = best_move(board)
-        print(move)
-        with open("Chessfile.txt", "a") as f:
-            f.write(str(board.san(move)))
-            f.write('|')
-        board.push(move)
+import sys
+from tqdm import tqdm
+games = 100
+white_wins = 0
+black_wins = 0
+draws = 0
+for _ in tqdm(range(games),file=sys.stdout,colour="green"):
+    board = chess.Board()
+    while not board.is_game_over():
+        if board.turn:
+            move = best_move(board,3)
+            board.push(move)
+        else:
+            move = best_move(board,4)
+            board.push(move)
 
-    else:
-        move = get_human_move(board)
-        print(move)
-        with open("Chessfile.txt", "a") as f:
-            f.write(str(board.san(move)))
-            f.write('|')
-        board.push(move)
+    outcome = board.outcome()
+    if outcome:
+        if outcome.result() == "1-0":
+            white_wins=white_wins + 1
+        elif outcome.result() == "0-1":
+            black_wins=black_wins + 1
+        else:
+            draws = draws + 1
 
-
-print(board)
-with open("Chessfile.txt", "a") as f:
-    f.write('   \n')
-print("Game Over")
-print(board.result())
-print(board.outcome())
-
-
+print(f"Results after {games} games:")
+print(f"White wins: {white_wins}")
+print(f"Black wins: {black_wins}")
+print(f"Draws: {draws}")
