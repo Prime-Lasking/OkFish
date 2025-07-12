3d9fd4bdced906e1449013c713887d65f8162c95
The Prime Mathematician <197593373+Prime-Lasking@users.noreply.github.com>
2025-07-12 12:45:44 +1000

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -24,28 +24,58 @@ def evaluation(board):
     wq = len(board.pieces(chess.QUEEN, chess.WHITE))
     bq = len(board.pieces(chess.QUEEN, chess.BLACK))
 
-    material_score = 1 * (wp - bp) + 3 * (wn - bn) + 4 * (wb - bb) + 5 * (wr - br) + 10 * (wq - bq)
+    material_score = 1 * (wp - bp) + 3.3 * (wn - bn) + 3.5 * (wb - bb) + 5 * (wr - br) + 9 * (wq - bq)
     return material_score + check_mate
     if board.turn:
-        return -total_score
-    else:
         return total_score
-
+    else:
+        return -total_score
+def negamax(board, depth, alpha, beta, color):
+    if depth == 0 or board.is_game_over():
+        return color * evaluation(board)
+    
+    max_eval = float('-inf')
+    for move in board.legal_moves:
+        board.push(move)
+        if move == None:
+            break
+        eval = -negamax(board, depth - 1, -beta, -alpha, -color)
+        board.pop()
+        max_eval = max(max_eval, eval)
+        alpha = max(alpha, eval)
+        if alpha >= beta:
+            break
+    return max_eval
 def best_move(board):
-        best_score = -float('inf')
-        best_move = chess.E4
-
+    legal_moves = list(board.legal_moves)
+    alpha = float('-inf')
+    beta = float('inf')
+    color = 1 if board.turn else -1
+    best_score = float('-inf')
+    best_move_found = random.choice(legal_moves)
         for move in board.legal_moves:
             board.push(move)
-            score = evaluation(board)
-            board.pop()
-
-            if score >= best_score:
-                best_score = score
-                best_move = move
-        return best_move
+            score = -negamax(board, depth - 1, -beta, -alpha, -color)
+            if board.is_capture(move):
+             captured = board.piece_at(move.to_square)
+             taker = board.piece_at(move.from_square)
+             if captured and taker and piece_values[captured.piece_type] >= piece_values[taker.piece_type]:
+                score += 10
+        board.pop()
+            if score == best_score:
+              best_score = score
+              best_move_found = move
+    return best_move_found
+def random_move(board):
+    legal_moves = list(board.legal_moves)
+    if legal_moves:
+        return random.choice(legal_moves)
+    else:
+        return None
 def get_human_move(board):
-    #Get a move from  input
     human_move = input("Enter your move (in SAN format:eg,e4): ")
-    return board.parse_san(human_move)
+    try:
+      return board.parse_san(human_move)
+    except ValueError:
+        print("Invalid move.Try again.")
 
